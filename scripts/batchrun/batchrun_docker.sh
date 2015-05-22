63469e6a1ba06058cd77f65984fa0492439ee9fd
Samir Amin <samin1@canmedm5000939.KEYSTONE>
2015-05-22 01:09:04 -0500

    batchrun_docker.sh file had missing `mkdir -p ./logs` command which will break batch processing. Pending update: While using default `./scripts/batchrun/docker_batchrun_all.txt`, allow user to provide custom base directory instead of forcing to make `/data/vol2`.

--- scripts/batchrun/batchrun_docker.sh
+++ scripts/batchrun/batchrun_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #htseq docker batchrun script:
-#v 0.9.4p2 | 05-15 | github.com/dyndna
+#v 0.9.4p3 | 05-15 | github.com/dyndna
 #@sbamin
 
 LINESTART="$1"
@@ -12,9 +12,13 @@ if [ "$1" == "-h" ] || [ "$1" == "--h" ] || [ "$#" -lt 3 ]; then
 	echo "Require three arguments in following order:"
 	echo -e "Usage: $(basename $0) <first line to read> <last line to read> <path to batchfile>\n"
 	echo -e "Example: $(basename $0) 201 205 ./scripts/batchrun/docker_batchrun_all.txt\n"
+	echo -e "If using default ./scripts/batchrun/docker_batchrun_all.txt, make sure you have workdir or git repo at /data/vol2/pcawg14_htseq\n"
+
 	exit 0
 else
 	n=1
+	mkdir -p ./logs
+
 	while read line; do 
 	    if [[ ($n -ge "$LINESTART") && ($n -le "$LINEEND")  ]]; then 
 	        
@@ -49,4 +53,3 @@ fi
 #   DIFF=$(( $END - $START ))
 #   echo "Download completed in $DIFF minutes"
 # done
-
